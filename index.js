25253865fe80b63dec0eb9fddd32101e1c2bd018
Natalia Lira <natalia.rlira@gmail.com>
2020-11-17 21:58:27 -0300

    node http - subindo servidor

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-//projeto global
+const http = require('http')
+
 const funcoes = require('./funcoes')
 
-funcoes.darBanhoPet()
+// http.createServer((req,res) => {
+
+// }).listen(3000, 'localhost',()=>{
+//     console.log("Servidor ON!")
+// })
+
+
+const server = http.createServer((req,res) => {
+
+})
+server.listen(3000, 'localhost',()=>{
+    console.log("Servidor ON!")
+})
+
+//Pratica I: 
+//- Criar servidor que escute na porta 3030;
+//- Criar rota '/' que nos liste todos os pets
